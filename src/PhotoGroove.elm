db9b60ea6e0d51c12a250c8be0a66b52edad1e53
Mario Mejia <mario@hackcapital.com>
2019-07-18 17:10:54 -0700

    add types, Cmd, events, random

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,65 +1,171 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onMouseOver)
+import Random
+
+
 
 -- helper function
-urlPrefix = 
-  "http://elm-in-action.com/"
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | GotSelectedIndex Int
+    | ClickedSurpriseMe
+
+
 
 {-
-  the view returns Html markup. by passign the model we connect the "state"
-  of the application with the markup.
+   the view returns Html markup. by passign the model we connect the "state"
+   of the application with the markup.
 -}
+
+
+view : Model -> Html Msg
 view model =
-  div [ class "content" ]
-    [ h1 [] [ text "Photo Groove" ]
-    , div [ id "thumbnails" ] 
-      (List.map (viewThumbnail model.selectedUrl) model.photos)
-    , img 
-      [ class "large"
-      , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-      ]
-      []
-    ]
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , button [ onClick ClickedSurpriseMe ] [ text "Surprise Me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+
 
 {-
-  helper function, which renders a single thumbnail as Html
-  Partially Applied function (curried)
+   helper function, which renders a single thumbnail as Html
+   Partially Applied function (curried)
 -}
-viewThumbnail selectedUrl thumb = 
-  img 
-    [ src (urlPrefix ++ thumb.url) 
-    , classList [ ( "selected", selectedUrl == thumb.url ) ]
-    , onClick { description = "ClickedPhoto", data = thumb.url }
-    ]
-    []
-  
 
 
-initialModel = 
-  { photos = 
-    [ { url= "1.jpeg" }
-    , { url= "2.jpeg" }
-    , { url= "3.jpeg" }
-    ]
-  , selectedUrl = "1.jpeg"
-  }
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick (ClickedPhoto thumb.url)
+        ]
+        []
 
-{-
-  hooks up our view to the model "state"
--}
-update msg model = 
-  if msg.description == "ClickedPhoto" then
-    { model | selectedUrl = msg.data }  
-  else 
-    model
 
-main = 
-  Browser.sandbox
-    { init = initialModel
-    , view = view
-    , update = update
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label [ onClick (ClickedSize size) ]
+        [ input [ type_ "radio", name "size", onMouseOver (ClickedSize size) ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+chosenSize : ThumbnailSize
+chosenSize =
+    Small
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
     }
+
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
+    }
+
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
+
+
+{-
+   hooks up our view to the model "state"
+-}
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedPhoto url ->
+            ( { model | selectedUrl = url }, Cmd.none )
+
+        ClickedSize size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
+        ClickedSurpriseMe ->
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
+
+        GotSelectedIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \flags -> ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \model -> Sub.none
+        }
